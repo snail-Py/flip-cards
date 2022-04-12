08b472283092c13c68852af89d937ec67ea3a972
snail-Py <cs.legoff@gmail.com>
2022-04-12 18:11:09 +0800

    Creating flip cards

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <h1>My first flash cards</h1>
+  
+  );
+}
+
+export default App;
